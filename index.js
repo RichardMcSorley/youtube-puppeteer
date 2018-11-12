39c8562dda63a8aa0eda81f2fc1a4a96025008a8
Richard McSorley <yourevilmonk@gmail.com>
2018-11-11 18:23:30 -0600

    bug

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const init = async () => {
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 };
+
 process.on("unhandledRejection", err => {
   console.log(err);
   process.exit(1);
