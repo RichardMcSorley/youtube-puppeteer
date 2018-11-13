545ccb262eab58c3af982940c8cd8cc2f89f2b49
Richard McSorley <yourevilmonk@gmail.com>
2018-11-13 01:11:39 -0600

    updated hooks

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,18 +1,12 @@
 var gith = require("gith").create(6000); // run on port 6000
-const git = require("simple-git");
-const path = "/root/puppet";
 
 gith({
   repo: "RichardMcSorley/youtube-puppeteer" // the github-user/repo-name
 }).on("all", function(payload) {
   console.log("push received");
-  git(path).raw(
-    ["config", "--global", "advice.pushNonFastForward", "false"],
-    (err, result) => {
-      if (err) {
-        return console.log("err", err);
-      }
-      console.log("pulled", result);
-    }
-  );
+  exec('/root/puppet/hooks/hooks.sh ' + payload.branch, function(err, stdout, stderr){
+    if (err) return err;
+    console.log(stdout);
+    console.log("git deployed to branch " + payload.branch);
+  });
 });
