0e70b7dcce49cbb8086a709b624b24279dbe5644
Richard McSorley <yourevilmonk@gmail.com>
2018-11-13 23:06:55 -0600

    updated hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
+
+pm2 stop all
+
 cd /root/puppet
 
 git fetch --all
 
-git reset --hard origin/master
+git reset --hard origin master
 
-git pull origin/master
+git pull origin master
 
 # Remove NPM packages
 
 npm uninstall `ls -1 node_modules | tr '/\n' ' '`
 
+rm -rf node_modules
+
 npm install #install new npm libraries
+
+pm2 start all
 
 return
