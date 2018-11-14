f0b0e6217fcd2ef1e67549d5b3427a369f845f2c
Richard McSorley <yourevilmonk@gmail.com>
2018-11-13 22:18:17 -0600

    prod

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -2,7 +2,7 @@
 
 git pull "origin" $1 #$1 is branch
 
-rm -rf node_modules # remove any unwanted libraries
+#rm -rf node_modules # remove any unwanted libraries
 
 npm install #install new npm libraries
 
