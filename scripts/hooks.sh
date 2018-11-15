7b6712e198d8d7cbaf5e6ee997c599d0edf9b466
Richard McSorley <yourevilmonk@gmail.com>
2018-11-15 01:36:35 -0600

    fixing hooks

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -14,9 +14,10 @@ git pull
 
 #npm uninstall `ls -1 node_modules | tr '/\n' ' '`
 #
-#rm -rf node_modules
 
-#npm install #install new npm libraries
+rm -rf node_modules
+
+npm install #install new npm libraries
 
 #pm2 start 1 
 
